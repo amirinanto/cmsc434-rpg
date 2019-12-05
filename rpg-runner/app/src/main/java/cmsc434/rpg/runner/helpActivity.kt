9d9c9dcd8452b189f675103c4cf53f9120216344
djanderson12044 <djanderson12044@gmail.com>
2019-12-05 10:26:26 -0500

    barebones options and help buttons

---
+++ rpg-runner/app/src/main/java/cmsc434/rpg/runner/helpActivity.kt
@@ -0,0 +1,15 @@
+package cmsc434.rpg.runner
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_help.*
+
+class helpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help)
+        setSupportActionBar(toolbar)
+    }
+}
